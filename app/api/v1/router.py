ef26e02c4d1bc1abf9eddbda43547f2588d590e9
pavel <prohorenkopavel74@gmail.com>
2022-05-20 16:43:32 +0500

    add developer endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import login, user
+from app.api.v1.endpoints import developer, login, user
 
 api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(developer.router, prefix="/developers", tags=["developers"])
