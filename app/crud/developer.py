8612079df0c5d536317537671fc18a8ad06a603e
pavel <prohorenkopavel74@gmail.com>
2022-05-20 16:24:23 +0500

    add developer CRUD

---
+++ app/crud/developer.py
@@ -0,0 +1,15 @@
+from typing import Any, Optional, Union
+
+from databases import Database
+
+from app.crud.base import CRUDBase
+from app.models import developer
+from app.schemas.developer import DeveloperIn, DeveloperUpdate
+
+
+class CRUDDeveloper(CRUDBase[type(developer), DeveloperIn, DeveloperUpdate]):
+    async def get_by_international_name(self, db: Database, *, international_name: str) -> Optional[Any]:
+        return await db.fetch_one(
+            self.model.select().where(self.model.c.international_name == international_name)
+        )
+
