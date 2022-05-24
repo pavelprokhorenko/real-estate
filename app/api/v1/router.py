5396628dc7bfc6abd07a702fb124a5aff5c8ca41
pavel <prohorenkopavel74@gmail.com>
2022-05-24 12:11:41 +0500

    add amenity endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import agent, developer, login, user
+from app.api.v1.endpoints import agent, amenity, developer, login, user
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(developer.router, prefix="/developers", tags=["developers"])
 api_router.include_router(agent.router, prefix="/agents", tags=["agents"])
+api_router.include_router(amenity.router, prefix="/amenities", tags=["amenities"])
