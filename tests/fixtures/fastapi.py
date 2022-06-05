e052c6f28e5e68f4db1e77b952ae3da89c0f1379
pavel <prohorenkopavel74@gmail.com>
2022-06-05 13:58:55 +0500

    add access token fixtures

--- tests/fixtures/fastapi.py
+++ tests/fixtures/fastapi.py
@@ -1,9 +1,13 @@
 from typing import Generator
 
 import pytest
+from databases import Database
 from fastapi.testclient import TestClient
 
+from app.core.config import settings
 from app.fastapi_app import app
+from tests.utils.user import authentication_token_from_email
+from tests.utils.utils import get_superuser_token_headers
 
 
 @pytest.fixture
@@ -15,3 +19,15 @@ def api_client() -> TestClient:
 def reset_dependency_overrides() -> Generator:
     yield
     app.dependency_overrides = {}
+
+
+@pytest.fixture
+def superuser_token_headers(api_client: TestClient) -> dict[str, str]:
+    return get_superuser_token_headers(api_client)
+
+
+@pytest.fixture
+def normal_user_token_headers(api_client: TestClient, db: Database) -> dict[str, str]:
+    return authentication_token_from_email(
+        api_client=api_client, email=settings.EMAIL_TEST_USER, db=db
+    )
